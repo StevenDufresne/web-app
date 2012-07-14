c234931545c66bb60b2967e2fde30143bf4f77da
StevenDufresne <stevedufresne2.0@gmail.com>
2012-07-13 21:01:19 -0400

    Some Styling changes to calendar.php

--- js/calendar.js
+++ js/calendar.js
@@ -1,10 +1,23 @@
 $(document).ready(function () {
+	var i = 0;
+	var rowSpans = $('.calendarBody'). find('[rowspan]');
 
-	$('.eventTime').on('click', function (ev) {
 
 
-		console.log(ev)
-;	});
+	rowSpans.each(function(){
+		var colNum = $(this).attr('rowspan'),
+			aLink = $(this).find('a'),
+			newHeight= 0;
+		(colNum > 3 ) ? newHeight = colNum * 40 : newHeight = colNum * 30
 
+		console.log(aLink.css('height', newHeight+'px'));
+
+		
+
+
+
+		
+		
+	})
 
 });
