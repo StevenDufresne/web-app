c05a3e3b5285eda463b9caa3a4569d96c116e51e
stevedufresne <stevedufresne2.0@gmail.com>
2012-07-16 12:44:31 -0400

    fixing db.

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -2,7 +2,7 @@
 
 $user = getenv('MYSQL_USERNAME');
 $pass = getenv('MYSQL_PASSWORD');
-$data_source = 'mysql:host=' . getenv('MYSQL_DB_HOST') . ';dbname=' . getenv('MYSQL_DB_NAME');
+$data_source = 'mysql:host=' . getenv('MYSQL_DB_HOST') . ';dbname=' . getenv('MYSQL_DB_NAME').'';
 
 //PDO -> php data objects
 $db = new PDO($data_source, $user, $pass);
