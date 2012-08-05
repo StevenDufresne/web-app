7a60021b17b239c423d5b9f4c713027c0cb8b209
StevenDufresne <stevedufresne2.0@gmail.com>
2012-08-04 20:52:57 -0400

    for phpog

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,10 +1,10 @@
 <?php
 
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$data_source = getenv('DATA_SOURCE');
+$user = getenv('MYSQL_USERNAME');
+$pass = getenv('MYSQL_PASSWORD');
+//$data_source = getenv('DATA_SOURCE');
 
-//$data_source = 'mysql:host=' . getenv('MYSQL_DB_HOST') . ';dbname=' . getenv('MYSQL_DB_NAME').'';
+$data_source = 'mysql:host=' . getenv('MYSQL_DB_HOST') . ';dbname=' . getenv('MYSQL_DB_NAME').'';
 //PDO -> php data objects
 $db = new PDO($data_source, $user, $pass);
 //Make the connection to speak in UTF 8 - don't expect anything back
