29837bcac73f4e25fa430343babc5586d1366775
stevedufresne <stevedufresn2.0@gmail.com>
2012-08-11 16:17:23 -0400

    Fixed connection for phpfog.

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -9,11 +9,11 @@
  */
 
 
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$data_source = getenv('DATA_SOURCE');
+$user = getenv('MYSQL_USERNAME');
+$pass = getenv('MYSQL_PASSWORD');
 
-//$data_source = 'mysql:host=' . getenv('MYSQL_DB_HOST') . ';dbname=' . getenv('MYSQL_DB_NAME').'';
+
+$data_source = 'mysql:host=' . getenv('MYSQL_DB_HOST') . ';dbname=' . getenv('MYSQL_DB_NAME').'';
 //PDO -> php data objects
 $db = new PDO($data_source, $user, $pass);
 //Make the connection to speak in UTF 8 - don't expect anything back
